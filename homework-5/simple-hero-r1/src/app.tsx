4f0bedc1655ec3832e4fe46e1dd831e17c65fbe7
Giedre <giedre.klima@gmail.com>
2022-08-31 15:20:04 +0300

    init simple-hero-r1

---
+++ homework-5/simple-hero-r1/src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <main>
+      <h1>Some header</h1>
+    </main>
+  );
+}
+
+export default App;
