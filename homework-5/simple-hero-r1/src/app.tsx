4dde523431b7e82b92856456b2cfa238e70b99c3
Giedre <giedre.klima@gmail.com>
2022-09-01 09:19:24 +0300

    add styles

--- homework-5/simple-hero-r1/src/app.tsx
+++ homework-5/simple-hero-r1/src/app.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-
-import './App.css';
-
+import * as React from 'react';
+import Hero from './components/hero';
+import SideBox from './components/side-box';
 
 const App: React.FC = () => {
+
   return (
     <main>
-      <h1>Some header</h1>
+      <Hero />
+      <SideBox />
     </main>
   );
 }
--- homework-5/simple-hero-r1/src/types/images.d.ts
+++ homework-5/simple-hero-r1/src/types/images.d.ts
@@ -1,4 +1,5 @@
-declare module '*.jpe?g';
+declare module '*.jpg';
+declare module '*.jpeg';
 declare module '*.png';
 declare module '*.gif';
 declare module '*.svg';
