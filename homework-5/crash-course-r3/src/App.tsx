dd2748273836f07e405f3bfd7c2c48a4b3b97421
Giedre <giedre.klima@gmail.com>
2022-09-04 23:20:54 +0300

    init, add header

---
+++ homework-5/crash-course-r3/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./components/header"
+
+import './App.css';
+
+function App() {
+  const name: string = 'BRAD';
+  return (
+    <div className="container">
+
+      <h1>Hello {name}</h1>
+      <Header />
+    </div >
+  );
+}
+
+export default App;
