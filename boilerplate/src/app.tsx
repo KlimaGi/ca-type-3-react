690ab5da587c30572c92c68fbc89b94f1453cde1
Giedre <giedre.klima@gmail.com>
2022-09-06 21:23:20 +0300

    feat: useState Example

--- boilerplate/src/app.tsx
+++ boilerplate/src/app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Container from './components/container';
+import CounterExample from './components/counter-example';
 
 const App: React.FC = () => (
   <main>
     <h3>Boilerplate</h3>
+    <Container>
+      <CounterExample />
+    </Container>
 
   </main>
 );
---
+++ boilerplate/src/components/counter-example.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const buttonStyle = {
+  fontSize: 30,
+  padding: '5px 20px',
+  cursor: 'pointer',
+};
+
+const CounterExample = () => {
+  const [count, setCount] = useState<number>(0);
+  const [step, setStep] = useState<number>(4);
+
+  return (
+    <div style={{ fontSize: 30, textAlign: 'center', marginTop: '2rem' }}>
+
+      <div>{`Count: ${count}`}</div>
+      <div style={{
+        display: 'flex', justifyContent: 'center', gap: 20, marginTop: '1rem',
+      }}
+      >
+        <input
+          type="number"
+          style={{ fontSize: 30, width: 140, textAlign: 'center' }}
+          value={step}
+          // Number(e.target.value)
+          onChange={(e) => setStep(e.target.valueAsNumber)}
+        />
+        <button
+          type="button"
+          style={buttonStyle}
+          onClick={() => setCount(count - step)}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          style={buttonStyle}
+          onClick={() => setCount(count + step)}
+        >
+          +
+        </button>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default CounterExample;
