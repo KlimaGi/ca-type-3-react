e1cc6db27d7e140d644de8b972b3e404033e2098
Giedre <giedre.klima@gmail.com>
2022-09-09 09:08:36 +0300

    feat: rangeInput change on FilledInput.onBlur e

--- ca-react-1/src/components/form-controls/range-field/components/range-input.tsx
+++ ca-react-1/src/components/form-controls/range-field/components/range-input.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FilledInput, FilledInputProps } from '@mui/material';
 
-type RangeInputProps = Omit<FilledInputProps, 'size'>;
+type RangeInputProps = Omit<FilledInputProps, 'size' | 'type' | 'multiline' | 'onChange' | 'value'> & {
+  value: number,
+  onChange: (e: React.FocusEvent<HTMLInputElement>, val: number) => void,
+};
 
 // sx - {} | FUN | Array< {} | FUN >
 
 // todo: Kviesti onChange prop'są tik tuomet, kai įvyksta eventas "onBlur"
 
-export const RangeInput: React.FC<RangeInputProps> = ({ sx = [], ...props }) => (
-  <FilledInput
-    size="small"
-    sx={[
-      {
-        flexGrow: 1,
-        input: {
-          pt: 1,
-          textAlign: 'center',
-        },
-      },
-      ...(sx instanceof Array ? sx : [sx]),
-    ]}
-    {...props}
-  />
-);
+export const RangeInput: React.FC<RangeInputProps> = ({
+  value,
+  onChange,
+  sx = [],
+  ...props
+}) => {
+  const [privateValue, setPrivateValue] = useState(value);
 
+  useEffect(() => {
+    setPrivateValue(value);
+  }, [value]);
+
+  return (
+    <FilledInput
+      size="small"
+      type="number"
+      multiline={false}
+      sx={[
+        {
+          flexGrow: 1,
+          input: {
+            pt: 1,
+            textAlign: 'center',
+          },
+        },
+        ...(sx instanceof Array ? sx : [sx]),
+      ]}
+      value={privateValue}
+      onChange={(e) => setPrivateValue(Number(e.target.value))}
+      onBlur={(e) => onChange(e as React.FocusEvent<HTMLInputElement>, Number(e.target.value))}
+      {...props}
+    />
+  );
+};
 export default RangeInput;
