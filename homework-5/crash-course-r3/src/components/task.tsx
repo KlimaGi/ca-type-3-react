441742d2ef143021b2c8053b87340d15cb3d3c43
Giedre <giedre.klima@gmail.com>
2022-09-12 18:37:13 +0300

    feat: add icon, chore: add react-icons library

--- homework-5/crash-course-r3/src/components/task.tsx
+++ homework-5/crash-course-r3/src/components/task.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 import styles from './task.module.scss';
 
 type TaskFCProp = ({ task: TaskProp });
 
 const Task: React.FC<TaskFCProp> = ({ task }) => (
   <div className={styles.task}>
-    <h3>{task.text}</h3>
+    <h3>
+      {task.text}
+      <FaTimes style={{ color: 'indianred', cursor: 'pointer' }} />
+    </h3>
     <p>{task.day}</p>
   </div>
 );
