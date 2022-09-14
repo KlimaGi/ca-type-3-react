b25a81e81f40b7ba3293a432483787eebf5aee5d
Giedre <giedre.klima@gmail.com>
2022-09-14 17:59:55 +0300

    feat: toggle reminder on mock server

--- homework-5/crash-course-r3/src/App.tsx
+++ homework-5/crash-course-r3/src/App.tsx
@@ -7,23 +7,6 @@ const App: React.FC = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
 
-  // Fetch Tasks
-  const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks');
-    const data = await res.json();
-
-    return data;
-  };
-
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks();
-      setTasks(tasksFromServer);
-    };
-
-    getTasks();
-  }, []);
-
   // Add Task
   const addTask: subAddTaskProp = async (task: subTaskProp) => {
     const res = await fetch('http://localhost:5000/tasks', {
@@ -45,12 +28,50 @@ const App: React.FC = () => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const toggleReminder: toggleReminderProp = (id: number) => {
+  // Fetch Task
+  const fetchTask = async (id: number) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
+  const toggleReminder: toggleReminderProp = async (id: number) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(tasks.map((task) => (
       task.id === id
-        ? { ...task, reminder: !task.reminder }
+        ? { ...task, reminder: data.reminder }
         : task)));
   };
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  };
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
 
   return (
     <div className="container">
