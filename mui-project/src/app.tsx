a572a1c3329385a312d584a50132cb989dece61a
Giedre <giedre.klima@gmail.com>
2022-09-15 11:14:23 +0300

    feat: init mui project

---
+++ mui-project/src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <main>
+    <h3>Boilerplate</h3>
+
+  </main>
+);
+
+export default App;
---
+++ mui-project/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  // strictMode - rodo klaidas develop aplinkoj, buildinime - istrinamas automatiskai
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
