c821979cd30a29a1dd9ef60ea264dd1c87f815af
Giedre <giedre.klima@gmail.com>
2022-09-17 14:24:18 +0300

    feat: add right bar

--- mui-project/src/components/feed.tsx
+++ mui-project/src/components/feed.tsx
@@ -1,56 +1,16 @@
 import React from 'react';
 import {
-  Box, Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, Typography, Checkbox,
+  Box,
 } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import { Favorite, FavoriteBorder, Share } from '@mui/icons-material';
-
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Post from './post';
 
 const Feed = () => (
   <Box flex={4} p={2}>
 
-    <Card>
-      <CardHeader
-        avatar={(
-          <Avatar sx={{ bgcolor: 'indianred' }} aria-label="recipe">
-            R
-          </Avatar>
-        )}
-        action={(
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        )}
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="20%"
-        image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-
-        <IconButton aria-label="add to favorites">
-          <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: 'indianred' }} />} />
-
-        </IconButton>
-        <IconButton aria-label="share">
-          <Share />
-        </IconButton>
-
-      </CardActions>
-
-    </Card>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
 
   </Box>
 );
--- mui-project/src/components/right-bar.tsx
+++ mui-project/src/components/right-bar.tsx
@@ -1,14 +1,132 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+  Box,
+  Typography,
+  AvatarGroup,
+  Avatar,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Divider,
+} from '@mui/material';
 
 const RightBar = () => (
   <Box
-    bgcolor="lightcoral"
     flex={2}
     p={2}
     sx={{ display: { xs: 'none', sm: 'block' } }}
   >
-    Right bar
+    <Box position="fixed" width={300}>
+      <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
+      <AvatarGroup max={6}>
+        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+        <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/2.jpg" />
+        <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+        <Avatar alt="Agnes Walker" src="https://material-ui.com/static/images/avatar/6.jpg" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/7.jpg" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/8.jpg" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/9.jpg" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/10.jpg" />
+      </AvatarGroup>
+
+      <Typography variant="h6" fontWeight={100} mt={2} mb={2}> Latest Photos</Typography>
+
+      <ImageList cols={3} rowHeight={100} gap={5}>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c" alt="Coffee" />
+        </ImageListItem>
+
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1533827432537-70133748f5c8" alt="Hats" />
+        </ImageListItem>
+
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1558642452-9d2a7deb7f62" alt="Honey" />
+        </ImageListItem>
+
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1516802273409-68526ee1bdd6" alt="Basketball" />
+        </ImageListItem>
+
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1518756131217-31eb79b20e8f" alt="Fern" />
+        </ImageListItem>
+      </ImageList>
+
+      <Typography variant="h6" fontWeight={100} mt={2}> Latest Conversations</Typography>
+
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Brunch this weekend?"
+            secondary={(
+              <>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </>
+            )}
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/2.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Summer BBQ"
+            secondary={(
+              <>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+              </>
+            )}
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Oui Oui"
+            secondary={(
+              <>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Sandra Adams
+                </Typography>
+                {' — Do you have Paris recommendations? Have you ever…'}
+              </>
+            )}
+          />
+        </ListItem>
+      </List>
+
+    </Box>
   </Box>
 );
 
