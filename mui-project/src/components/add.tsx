3702379ff1b38f8188ae77d20c23ce82888277f6
Giedre <giedre.klima@gmail.com>
2022-09-18 21:36:46 +0300

    feat: add modal

--- mui-project/src/components/add.tsx
+++ mui-project/src/components/add.tsx
@@ -1,13 +1,63 @@
-import React from 'react';
-import { Fab, Tooltip } from '@mui/material';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
+import {
+  Box, Fab, Modal, Tooltip, Typography, styled, Avatar, TextField,
+} from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
 
-const AddEl = () => (
-  <Tooltip title="Delete" sx={{ position: 'fixed', bottom: 20, left: { xs: 'calc(50% - 20px)', md: 30 } }}>
-    <Fab color="primary" aria-label="add">
-      <AddIcon />
-    </Fab>
-  </Tooltip>
-);
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
 
+const UserBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  marginBottom: '20px',
+});
+
+const AddEl = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Delete"
+        sx={{ position: 'fixed', bottom: 20, left: { xs: 'calc(50% - 20px)', md: 30 } }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={1}>
+          <Typography variant="h6" color="gray" textAlign="center">Create post</Typography>
+          <UserBox>
+            <Avatar
+              src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant="subtitle2">John Doe</Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: '100%' }}
+            id="standard-multiline-static"
+            multiline
+            rows={4}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+        </Box>
+      </StyledModal>
+    </>
+  );
+};
 export default AddEl;
