8ec5086670ba1a93cab6673a729f870e1bcd9773
Giedre <giedre.klima@gmail.com>
2022-09-28 20:56:22 +0300

    feat: add formik

--- ca-react-1/src/App.tsx
+++ ca-react-1/src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 // import ShopPage from './pages/shop-page';
-import LoginPage from './pages/login-page';
+// import LoginPage from './pages/login-page';
+import TestPage from './pages/test-page';
 
 const App: React.FC = () => (
   <>
     {/* <ShopPage   /> */}
-    <LoginPage />
+    {/* <LoginPage /> */}
+    <TestPage />
   </>
 
 );
