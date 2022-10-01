74d5c9ec28d0a09052ea75312bdc95c86b82eab9
Giedre <giedre.klima@gmail.com>
2022-10-01 22:12:58 +0300

    feat: contextProvider written in context file for better concept segregation

--- ca-react-1/src/pages/shop-page/contexts/drawer-context.tsx
+++ ca-react-1/src/pages/shop-page/contexts/drawer-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useMemo } from 'react';
 
 type DrawerContextType = {
   open: boolean,
@@ -7,5 +7,25 @@ type DrawerContextType = {
 };
 
 const DrawerContext = createContext<DrawerContextType>({} as DrawerContextType);
+
+type DrawerProviderProps = {
+  children: React.ReactNode
+};
+
+export const DrawerProvider: React.FC<DrawerProviderProps> = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
+  const drawerContextValue = useMemo(() => ({
+    open,
+    openDrawer: () => setOpen(true),
+    closeDrawer: () => setOpen(false),
+  }), [open]);
+
+  return (
+    <DrawerContext.Provider value={drawerContextValue}>
+      {children}
+    </DrawerContext.Provider>
+  );
+};
 
 export default DrawerContext;
--- ca-react-1/src/pages/shop-page/index.tsx
+++ ca-react-1/src/pages/shop-page/index.tsx
@@ -1,29 +1,19 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
 import { Box } from '@mui/material';
 
 import ApplicationBar from './components/application-bar';
 import SideBar from './components/side-bar';
 import MainSection from './components/main-section';
-import DrawerContext from './contexts/drawer-context';
+import { DrawerProvider } from './contexts/drawer-context';
 
-const ShopPage = () => {
-  const [open, setOpen] = useState(false);
-
-  const drawerContextValue = useMemo(() => ({
-    open,
-    openDrawer: () => setOpen(true),
-    closeDrawer: () => setOpen(false),
-  }), [open]);
-
-  return (
-    <DrawerContext.Provider value={drawerContextValue}>
-      <Box sx={{ display: 'flex' }}>
-        <ApplicationBar />
-        <SideBar />
-        <MainSection />
-      </Box>
-    </DrawerContext.Provider>
-  );
-};
+const ShopPage: React.FC = () => (
+  <DrawerProvider>
+    <Box sx={{ display: 'flex' }}>
+      <ApplicationBar />
+      <SideBar />
+      <MainSection />
+    </Box>
+  </DrawerProvider>
+);
 
 export default ShopPage;
